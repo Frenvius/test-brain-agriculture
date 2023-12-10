3ab986ffa1b99b410fad579bb512934ddbd8a602
Frenvius <frenvius@gmail.com>
2023-12-10 01:54:19 -0300

    feat: Added action to "add new" button on producer list

--- pages/Producer/index.tsx
+++ pages/Producer/index.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { useTranslations } from 'next-intl';
+import { useRouter } from 'next/navigation';
 import { Table, Button, Typography } from 'antd';
 
 import { columns } from './constants';
@@ -12,7 +13,12 @@ import Breadcrumb from '~/components/commons/Breadcrumb';
 const { Title } = Typography;
 
 const Producer = ({ data }: ProducerProps) => {
+	const router = useRouter();
 	const t = useTranslations('producers');
+
+	const handleNewProducer = () => {
+		router.push('/producers/add');
+	};
 
 	return (
 		<div className={styles.container}>
@@ -21,7 +27,9 @@ const Producer = ({ data }: ProducerProps) => {
 				<Title level={2} className={styles.title}>
 					{t('title')}
 				</Title>
-				<Button type="primary">{t('add')}</Button>
+				<Button type="primary" onClick={() => handleNewProducer()}>
+					{t('add')}
+				</Button>
 			</div>
 			<Table rowKey={(record) => record.id} columns={columns(t)} dataSource={data} />
 		</div>
